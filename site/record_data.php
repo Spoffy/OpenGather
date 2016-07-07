7d2c08bebbe39b198e4ba3547277b8ac4c1e1b52
Spoffy <spoffeh@gmail.com>
2016-07-07 17:37:03 +0100

    Record data accepts incoming POST data entries and records them to the MySQL server

--- site/record_data.php
+++ site/record_data.php
@@ -1,2 +1,47 @@
 <?php
-header($_SERVER["SERVER_PROTOCOL"]." 403 Forbidden", true, 403);
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+$createDatabaseQuery = "CREATE DATABASE IF NOT EXISTS open_data";
+
+$createTableQuery = <<< DB
+CREATE TABLE IF NOT EXISTS `open_data`.`app_data` ( 
+`time` BIGINT NOT NULL , 
+`label` TEXT NOT NULL , 
+`type` TEXT NOT NULL , 
+`latitude` DOUBLE NOT NULL , 
+`longitude` DOUBLE NOT NULL , 
+`accuracy` FLOAT NOT NULL )
+ ENGINE = CSV;
+DB;
+
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=open_data", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $conn->exec($createDatabaseQuery);
+    $conn->exec($createTableQuery);
+
+    $addDataQuery = "INSERT INTO open_data.app_data VALUES (:time, :label, :type, :lat, :long, :accuracy)";
+
+    $addDataStatement = $conn->prepare($addDataQuery);
+
+    $params = [
+        ":time" => $_POST["time"],
+        ":label" => $_POST["label"],
+        ":type" => $_POST["type"],
+        ":lat" => $_POST["position"]["lat"],
+        ":long" => $_POST["position"]["long"],
+        ":accuracy" => $_POST["position"]["accuracy"]
+    ];
+
+    print($addDataStatement->execute($params));
+} catch(PDOException $e) {
+    error_log($e->getMessage());
+    header($_SERVER['SERVER_PROTOCOL'] . '405 Internal Server Error');
+    print("MySQL Database error. See PHP error log");
+    die();
+}
