1dfb69a95a9987f5062cf597d933a7af52a265ad
Spoffy <spoffeh@gmail.com>
2016-08-04 21:00:30 +0100

    Added TODOs to Config

--- config.php
+++ config.php
@@ -1,4 +1,7 @@
 <?php
+//TODO Define a basepath in
+//TODO Add autoloader for Config
+//TODO Make Config a class
 define("CONFIG_MYSQL_HOST", "localhost");
 define("CONFIG_MYSQL_DB", "open_data");
 define("CONFIG_MYSQL_USERNAME", "root");
