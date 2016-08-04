6e929bd8b0ef57f2835adf7ad2005896059092c0
Spoffy <spoffeh@gmail.com>
2016-08-04 14:50:32 +0100

    Factored out database code

---
+++ site/database.php
@@ -0,0 +1,48 @@
+<?php
+
+//Contains all the query constants.
+//This is done so we don't need "global" every time we use one.
+//TODO Make all of these constants
+class DBQueries
+{
+    public static $createDatabase = "CREATE DATABASE IF NOT EXISTS open_data";
+    public static $createTables = <<< DB
+CREATE TABLE IF NOT EXISTS `open_data`.`app_data` ( 
+`time` BIGINT NOT NULL , 
+`label` TEXT NOT NULL , 
+`type` TEXT NOT NULL , 
+`latitude` DOUBLE NOT NULL , 
+`longitude` DOUBLE NOT NULL , 
+`accuracy` FLOAT NOT NULL )
+ ENGINE = CSV;
+DB;
+    public static $addDataQuery = "INSERT INTO open_data.app_data VALUES (:time, :label, :type, :lat, :long, :accuracy)";
+}
+
+class Database {
+    public $conn;
+
+    public function connect() {
+        $this->conn = new PDO("mysql:host=" . CONFIG_MYSQL_HOST . ";dbname=" . CONFIG_MYSQL_DB,
+            CONFIG_MYSQL_USERNAME,
+            CONFIG_MYSQL_PASSWORD);
+        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $this->conn->exec(DBQueries::$createDatabase);
+        $this->conn->exec(DBQueries::$createTables);
+
+        return $this;
+    }
+
+    public static function createAndConnect() {
+        $database = new Database();
+        $database->connect();
+        return $database;
+    }
+
+    public function addData($params) {
+        $statement = $this->conn->prepare(DBQueries::$addDataQuery);
+        print("Adding data");
+        $statement->execute($params);
+    }
+}
--- site/record_data.php
+++ site/record_data.php
@@ -1,34 +1,9 @@
 <?php
-
-$servername = "localhost";
-$username = "root";
-$password = "";
-
-$createDatabaseQuery = "CREATE DATABASE IF NOT EXISTS open_data";
-
-$createTableQuery = <<< DB
-CREATE TABLE IF NOT EXISTS `open_data`.`app_data` ( 
-`time` BIGINT NOT NULL , 
-`label` TEXT NOT NULL , 
-`type` TEXT NOT NULL , 
-`latitude` DOUBLE NOT NULL , 
-`longitude` DOUBLE NOT NULL , 
-`accuracy` FLOAT NOT NULL )
- ENGINE = CSV;
-DB;
-
+$base_path = dirname(__DIR__);
+require_once("$base_path/config.php");
+require_once("database.php");
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=open_data", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    $conn->exec($createDatabaseQuery);
-    $conn->exec($createTableQuery);
-
-    $addDataQuery = "INSERT INTO open_data.app_data VALUES (:time, :label, :type, :lat, :long, :accuracy)";
-
-    $addDataStatement = $conn->prepare($addDataQuery);
-
     $params = array(
         ":time" => $_POST["time"],
         ":label" => $_POST["label"],
@@ -45,10 +20,12 @@ try {
         $params[":accuracy"] = -1;
     }
 
-    print($addDataStatement->execute($params));
+    $database = Database::createAndConnect();
+    $database->addData($params);
 } catch(PDOException $e) {
     error_log($e->getMessage());
-    header($_SERVER['SERVER_PROTOCOL'] . '500 Internal Server Error');
     print("MySQL Database error. See PHP error log");
     die();
+} catch(Error $e) {
+    error_log("Hello!");
 }
