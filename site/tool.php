f95a195aad693690204da67ac43a4f5dfbf016de
Spoffy <spoffeh@gmail.com>
2016-08-08 17:34:42 +0100

    Added minified JQuery, moved to version 3.1.0 of JQuery

--- site/tool.php
+++ site/tool.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width,initial-scale=1" />
 
     <link rel="stylesheet" type="text/css" href="css/tool.css" media="all" />
-    <script type="text/javascript" src="js/jquery-3.0.0.js"></script>
+    <script type="text/javascript" src="js/jquery-3.1.0.min.js"></script>
 
     <link rel="stylesheet" type="text/css" href="leaflet/leaflet.css" />
     <script type="text/javascript" src="leaflet/leaflet.js"></script>
