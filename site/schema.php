6d9f421fbd311be8d628c7d1d24ada8fbf3f46ee
Spoffy <spoffeh@gmail.com>
2016-08-08 16:13:00 +0100

    Added Water, Shower and Point of Service schemas

--- site/schema.php
+++ site/schema.php
@@ -69,7 +69,7 @@ class DropdownField extends Field {
     }
 }
 
-//TODO Add CSS highlighting to readonly fields
+//TODO Replace/Add fields for Lat,Long with pre-specified IDs.
 class GeoField extends Field {
     public function buildFormField()
     {
@@ -85,6 +85,7 @@ class GeoField extends Field {
     }
 }
 
+//TODO Validate and check for identical IDs within schema. Two fields shouldn't be nameable the same.
 class ObjectSchema {
     public $name;
     public $id;
@@ -142,18 +143,56 @@ class ObjectSchema {
 
 //TODO Make JS validate required fields.
 $accessOptions = array("Unknown", "Open", "Card Scanner", "Key");
-$entranceSchema = new ObjectSchema("Building Entrance", array(
-    new TextField("Building Number", "buildingId", false),
-    new TextField("Entrance Label", "entranceId", false),
-    new TextField("Description", "description", false),
-    new GeoField("Latitude", "lat", false),
-    new GeoField("Longitude", "long", false),
-    new DropdownField("Access Method Daytime", "accessDaytime", $accessOptions),
-    new DropdownField("Access Method Evening", "accessEvening", $accessOptions)
-));
+$schemas = array(
+    new ObjectSchema("Building Entrance", array(
+        new TextField("Building Number", "buildingId", false),
+        new TextField("Entrance Label", "entranceId", false),
+        new TextField("Description", "description", false),
+        new GeoField("Latitude", "lat", false),
+        new GeoField("Longitude", "long", false),
+        new DropdownField("Access Method Daytime", "accessDaytime", $accessOptions),
+        new DropdownField("Access Method Evening", "accessEvening", $accessOptions),
+        new DropdownField("Opening Method", "openingMethod", array("Manual - Push/Pull", "Sensor + Automatic", "Pushbutton + Automatic"))
+    )),
 
-$otherSchema = new ObjectSchema("Other", array(
-    new TextField("Description", "description", false)
-));
+    new ObjectSchema("Image (Building)", array(
+        new TextField("Building Number", "buildingId", false),
+        new GeoField("Latitude", "lat", false),
+        new GeoField("Longitude", "long", false)
+    )),
 
-$schemas = array($entranceSchema, $otherSchema);
+    new ObjectSchema("Drinking Water Source", array(
+        new TextField("Building Number", "buildingId", false),
+        new TextField("Floor", "floor", false),
+        new GeoField("Latitude", "lat", false),
+        new GeoField("Longitude", "long", false)
+    )),
+
+    new ObjectSchema("Public Showers", array(
+        new TextField("Building Number", "buildingId", false),
+        new TextField("Floor", "floor", false),
+        new TextField("Room Number", "roomId", false),
+        new GeoField("Latitude", "lat", false),
+        new GeoField("Longitude", "long", false)
+    )),
+
+    new ObjectSchema("Point of Service", array(
+        new TextField("Description", "description", false),
+        new TextField("Building Number (If Applicable)", "buildingId", false),
+        new GeoField("Latitude", "lat", false),
+        new GeoField("Longitude", "long", false),
+        new TextField("Phone", "phone", false),
+        new TextField("Email", "email", false),
+        new TextField("Opening Hours: Mon", "monOpening", false),
+        new TextField("Opening Hours: Tue", "tueOpening", false),
+        new TextField("Opening Hours: Wed", "wedOpening", false),
+        new TextField("Opening Hours: Thur", "thurOpening", false),
+        new TextField("Opening Hours: Fri", "friOpening", false),
+        new TextField("Opening Hours: Sat", "satOpening", false),
+        new TextField("Opening Hours: Sun", "sunOpening", false)
+    )),
+
+    new ObjectSchema("Other", array(
+        new TextField("Description", "description", false),
+    ))
+);
