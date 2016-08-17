f5c75bc98f3147752bc994a8371d63cb177a1a7f
Spoffy <spoffeh@gmail.com>
2016-08-17 16:46:02 +0100

    [Refactor dumpjson.php] Tidied and made safer

--- site/dumpjson.php
+++ site/dumpjson.php
@@ -2,6 +2,8 @@
 $base_path = dirname(__DIR__);
 require_once("$base_path/src/schema.php");
 require_once("$base_path/src/database.php");
+require_once("$base_path/src/functional/pluck.php");
+require_once("$base_path/src/functional/first.php");
 
 //TODO Make this functional/neater
 //TODO Made this safer
@@ -9,17 +11,15 @@ $queryParams = array();
 parse_str($_SERVER['QUERY_STRING'], $queryParams);
 
 $database = Database::createAndConnect();
-$output = [];
+$output = array();
 
-if(key_exists("id", $queryParams)) {
-    $result = null;
-    foreach($schemas as $schema) {
-        if(strtolower($schema->id) == strtolower($queryParams["id"])) {
-            $result = $schema;
-        }
-    }
-    if(!$result) { die(); }
-    $output = $database->retrieveData($result);
+//Attempting to select results for only a single schema
+if(key_exists("schema", $queryParams)) {
+    $schemaId = $queryParams["schema"];
+    //First schema where the id equals given id.
+    $schema = first($schemas, function($schema) {global $schemaId; return $schema->id == $schemaId;});
+    if(!$schema) { die(); }
+    $output = $database->retrieveData($schema);
 } else {
     foreach($schemas as $schema) {
         $output = array_merge($output, $database->retrieveData($schema));
